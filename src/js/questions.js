d7cffe7f1faf465d165ee6cfabe1c1593bbd9e39
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-03-17 06:35:36 -0700

    Use indices to denote correct answers

--- src/js/questions.js
+++ src/js/questions.js
@@ -4,7 +4,7 @@ const QUESTIONS = [
     {
         question: "What is the capital city of Scotland?",
         options: ["London", "Edinburgh", "Glasgow", "Loch Ness"],
-        correctAnswer: "edinburgh",
+        correctAnswerIndex: 1,
         imageSrc: "https://travel-quiz.s3-us-west-1.amazonaws.com/questionImages/scotland.jpg" ,
         imageAlt: "The Scottish capital",
         id: cuid()
@@ -12,7 +12,7 @@ const QUESTIONS = [
     {
         question: "In which city could you find the famous landmark Christ the Redeemer?",
         options: ["Rio de Janeiro", "Caracas", "Mexico City", "Bogota"],
-        correctAnswer: "rio de janeiro",
+        correctAnswerIndex: 1,
         imageSrc: "https://travel-quiz.s3-us-west-1.amazonaws.com/questionImages/christ.jpg",
         imageAlt: "Christ the Redeemer",
         id: cuid()
@@ -20,7 +20,7 @@ const QUESTIONS = [
     {
         question: "Patagonia is a geographical region shared by Argentina and which other south American country?",
         options: ["Peru", "Uruguay", "Chile", "Brazil"],
-        correctAnswer: "chile",
+        correctAnswerIndex: 2,
         imageSrc: "https://travel-quiz.s3-us-west-1.amazonaws.com/questionImages/patagonia.jpeg",
         imageAlt: "Mountain range in Patagonia",
         id: cuid()
@@ -28,7 +28,7 @@ const QUESTIONS = [
     {
         question: "Tenzing-Hillary airport, rated by pilots as one of the most dangerous in the world, can be found in which country?",
         options: ["Colombia","Belize","Iceland","Nepal"],
-        correctAnswer: "nepal",
+        correctAnswerIndex: 3,
         imageSrc: "https://travel-quiz.s3-us-west-1.amazonaws.com/questionImages/airport.jpg",
         imageAlt: "A runway at Tenzing-Hillary airport",
         id: cuid()
@@ -36,7 +36,7 @@ const QUESTIONS = [
     {
         question: "The colourful Chichicastenango market can be found in which country?",
         options: ["Guatemala", "Morocco", "Japan", "Uzbekistan"],
-        correctAnswer: "guatemala",
+        correctAnswerIndex: 0,
         imageSrc: "https://travel-quiz.s3-us-west-1.amazonaws.com/questionImages/market.jpg",
         imageAlt: "Bright and colourful masks at Chichicastenango market",
         id: cuid()
@@ -44,7 +44,7 @@ const QUESTIONS = [
     {
         question: "In terms of the geographical size, what is the largest US state?",
         options: ["Rhode Island","Alaska","California","Texas"],
-        correctAnswer: "alaska",
+        correctAnswerIndex: 1,
         imageSrc: "https://travel-quiz.s3-us-west-1.amazonaws.com/questionImages/usa.jpg",
         imageAlt: "US flags blowing in the wind",
         id: cuid()
@@ -52,7 +52,7 @@ const QUESTIONS = [
     {
         question: "The famous blue city, Chefchaouen, can be found in which country?",
         options: ["Greece","Croatia","Morocco","France"],
-        correctAnswer: "morocco",
+        correctAnswerIndex: 2,
         imageSrc: "https://travel-quiz.s3-us-west-1.amazonaws.com/questionImages/blue.jpg",
         imageAlt: "Blue steps in the heart of Chefchaouen city",
         id: cuid()
@@ -60,7 +60,7 @@ const QUESTIONS = [
     {
         question: "How many official languages are spoken in Switzerland?",
         options: ["1","2","3","4"],
-        correctAnswer: "4",
+        correctAnswerIndex: 3,
         imageSrc: "https://travel-quiz.s3-us-west-1.amazonaws.com/questionImages/switzerland.png",
         imageAlt: "Swiss flag blowing in the wind in front of mountains",
         id: cuid()
@@ -68,7 +68,7 @@ const QUESTIONS = [
     {
         question: "What is the capital of Australia?",
         options: ["Canberra","Sydney","Melbourne","Adelaide"],
-        correctAnswer: "canberra",
+        correctAnswerIndex: 0,
         imageSrc: "https://travel-quiz.s3-us-west-1.amazonaws.com/questionImages/kangaroo.jpeg",
         imageAlt: "Kangaroo looking at the cameras",
         id: cuid()
@@ -76,7 +76,7 @@ const QUESTIONS = [
     {
         question: "What is the capital of Finland?",
         options: ["Oulu","Helsinki","Lapland","Tampere"],
-        correctAnswer: "helsinki",
+        correctAnswerIndex: 1,
         imageSrc: "https://travel-quiz.s3-us-west-1.amazonaws.com/questionImages/finland.jpg",
         imageAlt: "Waterfront of the Finnish capital at sunset",
         id: cuid()
@@ -84,7 +84,7 @@ const QUESTIONS = [
     {
         question: "Which French city has the nickname 'The Pink City' thanks to its blushing pink-brick buildings?",
         options: ["Paris","Marseille","Toulouse","Bordeaux"],
-        correctAnswer: "toulouse",
+        correctAnswerIndex: 2,
         imageSrc: "https://travel-quiz.s3-us-west-1.amazonaws.com/questionImages/pinkcity.jpg",
         imageAlt: "Sunset in the 'Pink City'",
         id: cuid()
@@ -92,7 +92,7 @@ const QUESTIONS = [
     {
         question: "The ancient city of Constantinople is now known by what name?",
         options: ["Jordan","Moscow","Dubai","Istanbul"],
-        correctAnswer: "istanbul",
+        correctAnswerIndex: 3,
         imageSrc: "https://travel-quiz.s3-us-west-1.amazonaws.com/questionImages/constantinople.jpg",
         imageAlt: "View over the modern-day Constantinople",
         id: cuid()
@@ -100,7 +100,7 @@ const QUESTIONS = [
     {
         question: "South Africa land locks which country?",
         options: ["Lesotho","Namibia","Ethiopia","Ghana"],
-        correctAnswer: "lesotho",
+        correctAnswerIndex: 0,
         imageSrc: "https://travel-quiz.s3-us-west-1.amazonaws.com/questionImages/southafrica.jpg",
         imageAlt: "An elephant and her two babies walk across a plain",
         id: cuid()
@@ -108,7 +108,7 @@ const QUESTIONS = [
     {
         question: "New Zealand has the highest sheep to person ratio in the world, with how many sheep per one person?",
         options: ["3","6","9","12"],
-        correctAnswer: "9",
+        correctAnswerIndex: 2,
         imageSrc: "https://travel-quiz.s3-us-west-1.amazonaws.com/questionImages/sheep.jpeg",
         imageAlt: "Herd of sheep in New Zealand",
         id: cuid()
@@ -116,7 +116,7 @@ const QUESTIONS = [
     {
         question: "How many writing systems does Japan have?",
         options: ["1","2","3","4"],
-        correctAnswer: "3",
+        correctAnswerIndex: 2,
         imageSrc: "https://travel-quiz.s3-us-west-1.amazonaws.com/questionImages/japanese.jpeg",
         imageAlt: "Japanese writing on paper",
         id: cuid()
